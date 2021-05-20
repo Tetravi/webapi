80da3636416c543760a78b4a76755fe031de334c
Yevheniia Poltorak <70563503+Tetravi@users.noreply.github.com>
2021-05-20 18:01:20 +0300

    Add files via upload

--- spec/jasmine_examples/test.js
+++ spec/jasmine_examples/test.js
@@ -8,90 +8,31 @@ let TeamCityReporter = new reporters.TeamCityReporter ({
 
 jasmine.getEnv().addReporter(TeamCityReporter)
 
+let Request = require('./../Request');
+
 describe("Upload file to dropbox", function() {
-  let axios = require('axios');
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  let postMethod = {
-      method: 'post',
-      url: 'https://content.dropboxapi.com/2/files/upload',
-      headers: { 
-       'Dropbox-API-Arg': '{"mode":"add","autorename":true,"mute":false,"path":"/hello.txt"}', 
-       'Content-Type': 'application/octet-stream'
-     },
-      data : {
-       binary: "/hello.txt"
-     }
-    };
+  let request = new Request(token,"post", "https://content.dropboxapi.com/2/files/upload",
+                               {'Dropbox-API-Arg': '{"mode":"add","autorename":true,"mute":false,"path":"/hello.txt"}',
+                                'Content-Type': 'application/octet-stream'},
+                               {binary: "/hello.txt"});
 
-  it("All loaded successfuly", async function() {
-      let responseStatus =0;
-      await axios(postMethod)
-      .then( function (response) {
-        responseStatus  = response.status;
-      })
-      .catch(function (error) {
-         console.log(error);
-      });
-
-  expect(responseStatus).toBe(200);
-    
-  }, 10000);
+  request.run("File loaded successfully");
 });
 
 describe("Get metadata", function(){
-  let axios = require('axios');
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  let getMethod = {
-       method: 'post',
-       url: 'https://api.dropboxapi.com/2/files/get_metadata',
-       headers: { 
-         'Content-Type': 'application/json', 
-         'Authorization': `Bearer ${token}`
-       },
-       data : {
-        "path":"/hello.txt"
-       }
-    };
+    let request = new Request(token, "post","https://api.dropboxapi.com/2/files/get_metadata",
+                                 {'Content-Type': 'application/json',
+                                  'Authorization': `Bearer ${token}`},
+                                 {"path":"/hello.txt"});
 
-  it("Metadata gotten successfuly", async function() {
-
-    let responseStatus =0;
-    await axios(getMethod)
-    .then(function (response) {
-      responseStatus = response.status;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-    expect(responseStatus).toBe(200);
-  }, 10000);
+    request.run("Metadata gotten successfully");
 });
 
 describe("Delete file", function(){
-  let axios = require('axios');
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    let request = new Request(token,'post','https://api.dropboxapi.com/2/files/delete_v2',
+                                 {'Authorization': `Bearer ${token}`,
+                                  'Content-Type': 'application/json'},
+                                 {"path":"/hello.txt"});
 
-  it("Deleted successfuly", async function() {
-    let deleteMethod = {
-    method: 'post',
-    url: 'https://api.dropboxapi.com/2/files/delete_v2',
-    headers: { 
-    'Authorization': `Bearer ${token}`, 
-    'Content-Type': 'application/json'
-    },
-    data : {
-        "path":"/hello.txt"
-    }
-};
-  let responseStatus =0;
-  await axios(deleteMethod)
-    .then(function (response) {
-      responseStatus = response.status;
-    })
-    .catch(function (error) {
-       console.log(error);
-    });
-  expect(responseStatus).toBe(200);    
-  }, 10000);
+    request.run("Deleted successfully");
 });
